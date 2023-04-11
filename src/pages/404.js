b10233ace46825722e81cd006505b83b87ef32d4
Jack Lee <welsar55@gmail.com>
2023-04-10 22:21:34 -0500

    add 404

---
+++ src/pages/404.js
@@ -0,0 +1,31 @@
+import Link from 'next/link'
+
+export default function NotFound() {
+  return (
+    <div style={{
+        display: "flex",
+        flexDirection: "column",
+        height: "100vh",
+        width: "100vw",
+        justifyContent: "center",
+        alignItems: "center"
+    }}>
+        <div style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "30%",
+            width: "30%",
+            backgroundColor: "white",
+            color: "black",
+            borderRadius: "40px",
+            padding: "40px"
+        }}>
+            <h1>404</h1>
+            <h2><a href="https://www.youtube.com/watch?v=tH2w6Oxx0kQ">All we are is dust in the wind...</a></h2>
+            <Link href="/">Landing Page</Link>
+        </div>
+    </div>
+  )
+}
