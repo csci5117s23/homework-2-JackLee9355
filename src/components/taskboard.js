adc5fc9101a499f477fe5603b915a2c1b01396d4
Jack Lee <welsar55@gmail.com>
2023-04-15 00:45:28 -0500

    Add changing categories

--- src/components/taskboard.js
+++ src/components/taskboard.js
@@ -1,6 +1,7 @@
 import './config';
 import { useState, useEffect } from "react"
 import Todo from './todo';
+import Categories from './categories';
 import { useAuth } from '@clerk/nextjs';
 import { useRouter } from 'next/router';
 import NotFound from '../pages/404';
@@ -8,6 +9,9 @@ import NotFound from '../pages/404';
 export default function Taskboard({ heading, filters, newTodos, category, id }) {
 
     const [todos, setTodos] = useState(null);
+    const [categories, setCategories] = useState([]);
+    const [newCategories, setNewCategories] = useState([]);
+    const [focused, setFocused] = useState(null);
     const { isLoaded, userId, sessionId, getToken } = useAuth();
     const router = useRouter();
 
@@ -31,28 +35,88 @@ export default function Taskboard({ heading, filters, newTodos, category, id }) 
         fetchData();
     }, [newTodos]); // So I should probably add these seperately. Or I can just refresh
 
+    const handleUpdateCategories = () => {
+        console.log("handlin~~!");
+        const update = {...focused}
+        update.categories = categories.map(category => category.name);
+
+        const fetchData = async () => {
+            const token = await getToken({ template: "codehooks" });
+            const response = await fetch(global.config.backend.apiUrl + "/todo/" + focused._id, {
+                method: "PUT",
+                headers: {
+                    'Authorization': 'Bearer ' + token,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(update)
+            });
+
+            setNewCategories([...newCategories, "UPDATE"]);
+        }
+        fetchData();
+    }
+
     const getById = (id) => {
         const todo = todos.find(todo => todo._id == id);
+        if (focused != todo) {
+            setFocused(todo);
+        }
 
         if (!todo) {
             router.push('/404');
-            return (
-                <NotFound />
-            )
+            return <NotFound />;
         }
 
         return (
-            <Todo
-                key={JSON.stringify(todo)} 
-                id={todo._id}
-                prio={todo.priority}
-                text={todo.text} 
-                status={todo.status} 
-                date={todo.dueDate}
-                categories={todo.categories}
-                focused={true}
-            />
-        )
+            <div style={{
+                display: "flex",
+                flexDirection: "column",
+                padding: "20px 40px 20px 20px",
+                width: "75%",
+            }}>
+                <Todo
+                    key={JSON.stringify(todo)} 
+                    id={todo._id}
+                    prio={todo.priority}
+                    text={todo.text} 
+                    status={todo.status} 
+                    date={todo.dueDate}
+                    categories={todo.categories}
+                    focused={true}
+                />
+                <div style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    width: "100%",
+                }}>
+                    <Categories
+                        setSelectedCategories={setCategories}
+                        newCategories={newCategories}
+                        containerStyle={{
+                            display: 'flex',
+                            flexDirection: 'row',
+                            height: 'fit-content',
+                            marginRight: '10px',
+                        }}
+                    />
+                    <button
+                        onClick={handleUpdateCategories}
+                        style={{
+                            backgroundColor: 'green',
+                            borderRadius: '5rem',
+                            height: '2.5rem',
+                            marginLeft: '0.5rem',
+                            border: 'none',
+                            color: 'white',
+                            fontSize: '1rem',
+                            fontWeight: 'bold',
+                        }}
+                    >
+                        Update Categories
+                    </button>
+                </div>
+            </div>
+        );
     }
 
     return (
@@ -106,5 +170,5 @@ export default function Taskboard({ heading, filters, newTodos, category, id }) 
                 }))}
             </div>
         </div>
-    )
+    );
 }
